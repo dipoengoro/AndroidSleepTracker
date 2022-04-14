f65e9825969c2c7ff51a6f55aeb73f0bc1fa915a
Muhammad Diponegoro <dipoengoro@outlook.com>
2022-04-14 21:12:32 +0700

    Step-3: Setup SleepTrackerViewModel

--- app/src/main/java/id/dipoengoro/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/id/dipoengoro/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import id.dipoengoro.sleeptracker.R
+import id.dipoengoro.sleeptracker.database.SleepDatabase
 import id.dipoengoro.sleeptracker.databinding.FragmentSleepTrackerBinding
+import kotlinx.coroutines.InternalCoroutinesApi
 
 class SleepTrackerFragment : Fragment() {
 
+    @InternalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,7 +23,16 @@ class SleepTrackerFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentSleepTrackerBinding>(
             inflater, R.layout.fragment_sleep_tracker, container, false
         )
-
+        val application = requireNotNull(this.activity).application
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+        val sleepTrackerViewModel = ViewModelProvider(this, viewModelFactory)[
+            SleepTrackerViewModel::class.java
+        ]
+        binding.apply {
+            this.sleepTrackerViewModel = sleepTrackerViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
         return binding.root
     }
 }
